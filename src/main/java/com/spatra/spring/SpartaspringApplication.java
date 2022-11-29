8257a2e35abe8f2af088deec9d85a40b7485cdaf
starMinK <rbals040329@gmail.com>
2022-11-29 20:00:34 +0900

    Initial commit

---
+++ src/main/java/com/spatra/spring/SpartaspringApplication.java
@@ -0,0 +1,48 @@
+package com.spatra.spring;
+
+import com.spatra.spring.domain.Course;
+import com.spatra.spring.domain.CourseRepository;
+import com.spatra.spring.domain.CourseRequestDto;
+import com.spatra.spring.service.CourseService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.List;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpartaspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaspringApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
+        return (args) -> {
+            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
+
+            System.out.println("데이터 인쇄");
+            List<Course> courseList = courseRepository.findAll();
+            for (int i=0; i<courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+            CourseRequestDto requestDto = new CourseRequestDto("웹개발의 봄, Spring", "임민영");
+            courseService.update(1L, requestDto);
+            courseList = courseRepository.findAll();
+            for (int i=0; i<courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+        };
+    }
+}
---
+++ src/test/java/com/spatra/spring/SpartaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spatra.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
